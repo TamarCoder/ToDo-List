7c768ddad828f63a09c8fc9f30bb557a81fd020d
Tamar khuskivadze (თამარ ხუსკივაძე) <thamuna.khuskivadze@gmail.com>
2025-03-30 20:04:53 +0400

    add my first app

---
+++ script.js
@@ -0,0 +1,70 @@
+const addBtn = document.getElementById('add-btn');
+const input = document.getElementById('input');
+const lists = document.getElementById('lists')
+
+function addButton(e) {
+    let inputValue = input.value.trim();
+    if (inputValue === "") return;  
+    //  შევქმენი list -  დივი. 
+    let taskDiv = document.createElement('div');
+    taskDiv.className = 'list';
+
+    // შევქმნეი  პირველ  დივი ტექტისთვის 
+    let textDiv = document.createElement('div');
+    textDiv.className = 'texts';
+    // შევქმნათ p თეგი სადაც ტექტსში შევიტანთ
+    let text =  document.createElement('li')
+    text.className = 'text'
+    text.textContent = inputValue;
+    //  მეორე დივი აიქონებისთვის 
+    let actionsDiv = document.createElement('div');
+    actionsDiv.className = 'icons';
+    //  შევქმნათ პირველი აიქონი
+    let edit =   document.createElement('i');
+    edit.className = 'bi bi-check-circle  edit';
+    // შევქმნათ მეორე აიქონი
+    let deleteIcon =   document.createElement('i');
+    deleteIcon.className = 'bi bi-x delete';
+
+    //   ახალი დივები დავამატო list - დივში
+    taskDiv.appendChild(textDiv);
+    taskDiv.appendChild(actionsDiv);
+    textDiv.appendChild(text);
+    actionsDiv.appendChild(edit)
+    actionsDiv.appendChild(deleteIcon)
+    //  ყოველი თასქი ემატება - მთავარ lists.
+     
+    function deleteTask(){
+        taskDiv.remove()
+    }
+    deleteIcon.addEventListener('click', deleteTask)
+
+   function textChange() {
+        text.style.color = 'rgb(222, 118, 8)'; // text-ი შეიცვლის ფერს
+    }
+
+    textDiv.addEventListener('click', textChange  => {
+        if (e.target !== text) {   
+            text.style.color = 'rgb(222, 118, 8)';  
+        }
+    });
+
+
+    function checkTask (){
+        text.style.textDecoration = 'line-through';
+        text.style.color = 'green'
+    }
+
+    edit.addEventListener('click', checkTask);
+
+
+
+
+
+
+    lists.appendChild(taskDiv);
+    input.value = '';
+}
+
+
+addBtn.addEventListener('click', addButton);
